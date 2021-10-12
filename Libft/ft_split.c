7259e91011a5ef767fca3ca00a14dc1a17bd8e8c
Polina Kleiner <polina.kleiner@gmail.com>
2021-10-12 12:58:55 +0300

    Libft completed

--- Libft/ft_split.c
+++ Libft/ft_split.c
@@ -6,103 +6,94 @@
 /*   By: dground <dground@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/10 00:05:49 by dground           #+#    #+#             */
-/*   Updated: 2021/10/11 21:32:22 by dground          ###   ########.fr       */
+/*   Updated: 2021/10/12 12:49:48 by dground          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-static int	ft_word_count(char const *s, char c)
+static size_t	ft_word_count(char const *s, char c)
 {
-	int	i;
-	int	count;
+	size_t	i;
+	size_t	count;
 
 	i = 0;
 	count = 0;
-	if (s && c)
+	while (s[i])
 	{
-		while (s[i])
-		{
-			while (s[i] == c)
-				i++;
-			if (s[i])
-				count++;
-			while (s[i] && s[i] != c)
-				i++;
-		}
+		while (s[i] == c)
+			i++;
+		if (s[i] && s[i] != c)
+			count++;
+		while (s[i] && s[i] != c)
+			i++;
 	}
 	return (count);
 }
 
-static char	*ft_strncpy(char *dest, const char *src, size_t n)
+static size_t	ft_word_length(char const *s, char c)
 {
 	size_t	i;
+	size_t	len;
 
 	i = 0;
-	while ((src[i] != '\0') && (i < n))
+	len = 0;
+	while (s[i] == c)
+		i++;
+	while (s[i] != c && s[i])
 	{
-		dest[i] = src[i];
+		len++;
 		i++;
 	}
-	while ((dest[i] != '\0') && (i < n))
-	{
-		dest[i] = '\0';
-		i++;
-	}
-	return (dest);
+	return (len);
 }
 
-static char	*ft_strndup(const char *s, size_t n)
-{
-	char	*str;
-
-	str = (char *)malloc(sizeof(char) * (n + 1));
-	if (str == NULL)
-		return (NULL);
-	str = ft_strncpy(str, s, n);
-	str[n] = '\0';
-	return (str);
-}
-
-/*static void	ft_free_error(char **result, size_t n)
+static void	ft_free_after_error(char **splitted, size_t n)
 {
 	while (n--)
 		free(splitted[n]);
 	free(splitted);
-}*/
+}
+
+static void	ft_go_to_next_word(char const *s, size_t *k, char c)
+{
+	while (s[*k] == c)
+		(*k)++;
+}
 
 typedef struct s_var
 {
 	char	**splitted;
-	int		i;
-	int		j;
-	int		k;
+	size_t	i;
+	size_t	j;
+	size_t	k;
 }	t_var;
 
 char	**ft_split(char const *s, char c)
 {
 	t_var	t;
 
-	t.i = 0;
-	t.k = 0;
 	if (!s)
 		return (NULL);
-	t.splitted = (char **)malloc(sizeof(char *) * (ft_word_count(s, c)) + 1);
+	t.splitted = (char **)malloc(sizeof(char *) * (ft_word_count(s, c) + 1));
 	if (!t.splitted)
 		return (NULL);
-	while (s[t.i])
+	t.i = 0;
+	t.k = 0;
+	while (t.i < ft_word_count(s, c))
 	{
-		while (s[t.i] == c)
-			t.i++;
-		t.j = t.i;
-		while (s[t.i] && s[t.i] != c)
-			t.i++;
-		if (t.i > t.j)
+		t.j = 0;
+		t.splitted[t.i] = (char *)malloc(ft_word_length(s + t.k, c) + 1);
+		if (!(t.splitted[t.i]))
 		{
-			t.splitted[t.k] = ft_strndup(s + t.j, t.i - t.j);
-			t.k++;
+			ft_free_after_error(t.splitted, t.i);
+			return (NULL);
 		}
+		ft_go_to_next_word(s, &t.k, c);
+		while (s[t.k] && s[t.k] != c)
+			t.splitted[t.i][t.j++] = s[t.k++];
+		t.splitted[t.i++][t.j] = '\0';
 	}
-	t.splitted[t.k] = NULL;
+	t.splitted[t.i] = NULL;
 	return (t.splitted);
 }
